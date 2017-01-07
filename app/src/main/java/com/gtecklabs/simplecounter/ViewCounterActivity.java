7320f5171e0ef8473d99d1a43c2841b0addec62c
Daniel Grech <dgrech@fb.com>
2017-01-06 21:07:49 -0800

    Toolbar for ViewCounterActivity

--- app/src/main/java/com/gtecklabs/simplecounter/ViewCounterActivity.java
+++ app/src/main/java/com/gtecklabs/simplecounter/ViewCounterActivity.java
@@ -2,15 +2,31 @@ package com.gtecklabs.simplecounter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import butterknife.BindView;
 import com.gtecklabs.simplecounter.di.DiComponent;
 import com.gtecklabs.simplecounter.foundation.BaseActivity;
+import com.gtecklabs.simplecounter.model.Count;
 
 public class ViewCounterActivity extends BaseActivity<ViewCounterActivity, ViewCounterPresenter> {
 
-  private static final String EXTRA_COUNT_ID = "count_id";
+  @BindView(R.id.collapsing_toolbar)
+  CollapsingToolbarLayout mCollapsingToolbar;
+
+  @BindView(R.id.toolbar_background_content)
+  View mToolbarBackgroundContent;
+
+  @BindView(R.id.toolbar)
+  Toolbar mToolbar;
 
   public static Intent createIntent(Context context, long id) {
-    return new Intent(context, ViewCounterActivity.class).putExtra(EXTRA_COUNT_ID, id);
+    return new Intent(context, ViewCounterActivity.class).putExtra(ViewCounterPresenter.EXTRA_COUNT_ID, id);
   }
 
   @Override
@@ -26,5 +42,33 @@ public class ViewCounterActivity extends BaseActivity<ViewCounterActivity, ViewC
   @Override
   protected ViewCounterPresenter createPresenter() {
     return new ViewCounterPresenter(this);
+  }
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setupToolbar();
+  }
+
+  void bind(Count count) {
+    mCollapsingToolbar.setTitle(count.title());
+    mCollapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
+    mCollapsingToolbar.setExpandedTitleColor(Color.WHITE);
+    setToolbarColor(count.color());
+  }
+
+  private void setToolbarColor(@ColorInt int color) {
+    mToolbarBackgroundContent.setBackgroundColor(color);
+    mCollapsingToolbar.setContentScrimColor(color);
+    getWindow().setStatusBarColor(color);
+  }
+
+  private void setupToolbar() {
+    mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        onBackPressed();
+      }
+    });
   }
 }
--- app/src/main/java/com/gtecklabs/simplecounter/ViewCounterPresenter.java
+++ app/src/main/java/com/gtecklabs/simplecounter/ViewCounterPresenter.java
@@ -1,10 +1,29 @@
 package com.gtecklabs.simplecounter;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.gtecklabs.simplecounter.data.CountLoader;
 import com.gtecklabs.simplecounter.di.ActivityModule;
 import com.gtecklabs.simplecounter.di.DiComponent;
 import com.gtecklabs.simplecounter.foundation.BaseActivityPresenter;
+import com.gtecklabs.simplecounter.foundation.BaseSubscriber;
+import com.gtecklabs.simplecounter.model.Count;
+import com.gtecklabs.simplecounter.util.Preconditions;
+import com.gtecklabs.simplecounter.util.Toaster;
+
+import javax.inject.Inject;
 
 public class ViewCounterPresenter extends BaseActivityPresenter<ViewCounterActivity> {
+
+  static final String EXTRA_COUNT_ID = "count_id";
+
+  @Inject
+  CountLoader mCountLoader;
+
+  @Inject
+  Toaster mToaster;
+
+  private long mCountId;
 
   public ViewCounterPresenter(ViewCounterActivity activity) {
     super(activity);
@@ -12,5 +31,32 @@ public class ViewCounterPresenter extends BaseActivityPresenter<ViewCounterActiv
 
   protected void inject(DiComponent component) {
     component.newActivityComponent(new ActivityModule(getActivity())).inject(this);
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle bundle) {
+    super.onCreate(bundle);
+
+    mCountId = getActivity().getIntent().getLongExtra(EXTRA_COUNT_ID, -1);
+    Preconditions.checkArgument(mCountId >=0 , "No count id passed in intent");
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    subscribe(
+        mCountLoader.getCount(mCountId),
+        new BaseSubscriber<Count>() {
+          @Override
+          public void onError(Throwable e) {
+            mToaster.toastLong(R.string.error_loading_count);
+            getActivity().finish();
+          }
+
+          @Override
+          public void onNext(Count count) {
+            getActivity().bind(count);
+          }
+        });
   }
 }
